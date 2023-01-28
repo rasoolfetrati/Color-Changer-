c37b9980aafc6a4bbed9584ec1382b229bc88c99
RF <RF@DESKTOP-FJ8TG9F>
2023-01-28 17:21:40 +0330

    Add project files.

---
+++ TestApp/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using CommunityToolkit.Maui.Alerts;
+
+namespace TestApp;
+
+public partial class MainPage : ContentPage
+{
+    bool isRandom;
+    string hexValue;
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+
+    private void slider_ValueChanged(object sender, ValueChangedEventArgs e)
+    {
+        if (!isRandom)
+        {
+            var red = sldRed.Value;
+            var grn = sldGreen.Value;
+            var blue = sldBlue.Value;
+
+            Color color = Color.FromRgb(red, grn, blue);
+            SetColor(color);
+        }
+    }
+
+    private void SetColor(Color color)
+    {
+        Container.BackgroundColor = color;
+        rndBtn.BackgroundColor = color;
+        hexValue = color.ToHex();
+        lblHex.Text = hexValue;
+    }
+
+    private void rndBtn_Clicked(object sender, EventArgs e)
+    {
+        isRandom = true;
+        var random = new Random();
+        Color color = Color.FromRgb(random.Next(0, 256), random.Next(0, 256), random.Next(0, 256));
+        SetColor(color);
+        sldRed.Value = color.Red;
+        sldGreen.Value = color.Green;
+        sldBlue.Value = color.Blue;
+        isRandom = false;
+    }
+
+    private async void ImageButton_Clicked(object sender, EventArgs e)
+    {
+        await Clipboard.SetTextAsync(hexValue);
+        var toast = Toast.Make("Color Copied!", CommunityToolkit.Maui.Core.ToastDuration.Long, 12);
+        await toast.Show();
+    }
+}
+
